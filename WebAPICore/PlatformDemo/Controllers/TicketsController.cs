4ae98fc1f9cdc1b5adab8716a7bcfc6df16c02ce
Rekenei Zolta <rekenei.zolta@gmail.com>
2021-08-04 12:04:33 +0200

    Web API Routing #2

--- WebAPICore/PlatformDemo/Controllers/TicketsController.cs
+++ WebAPICore/PlatformDemo/Controllers/TicketsController.cs
@@ -7,38 +7,34 @@ using System.Threading.Tasks;
 namespace PlatformDemo.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class TicketsController : ControllerBase
     {
         [HttpGet]
-        [Route("api/tickets")]
         public IActionResult Get()
         {
             return Ok("Reading all the tickets");
         }
 
-        [HttpGet]
-        [Route("api/ticket/{id}")]
+        [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
             return Ok($"Reading all the tickets #{id}");
         }
 
         [HttpPost]
-        [Route("api/tickets")]
         public IActionResult Post()
         {
             return Ok("Creating a ticket");
         }
 
         [HttpPut]
-        [Route("api/tickets")]
         public IActionResult Put()
         {
             return Ok("Update a ticket");
         }
 
-        [HttpDelete]
-        [Route("api/ticket")]
+        [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
             return Ok($"Deleteing a ticket #{id}");
