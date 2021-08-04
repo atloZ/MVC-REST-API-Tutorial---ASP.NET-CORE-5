32cb7d7cf340cf57b6b39ecc564784d31986a0ac
Rekenei Zolta <rekenei.zolta@gmail.com>
2021-08-04 12:10:34 +0200

    Practice Time: Web API Routing

---
+++ WebAPICore/PlatformDemo/Controllers/ProjectsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Reading all the projects");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Reading all the projects #{id}");
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Creating a project");
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Update a project");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Deleteing a project #{id}");
+        }
+    }
+}
